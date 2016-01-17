84465cf75624e771a6d164fc6bef2eb61a1ce64b
Igor Babuschkin <igor@babuschk.in>
2016-01-17 21:56:03 +0000

    Adjust edit command

--- cmd/edit.go
+++ cmd/edit.go
@@ -1,36 +1,36 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
+	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
+func Edit(identifier int) {
+
+}
+
 var editCmd = &cobra.Command{
-	Use:   "edit",
+	Use:   "edit <identifier>",
 	Short: "Edit a draft",
 	Long: ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) < 2 {
-			fmt.Println("Error: edit requires a draft identifier")
+		if len(args) < 1 {
+			fmt.Println("Error: Edit requires a draft identifier.")
 			os.Exit(-1)
 		}
-		fmt.Println("edit called with", args[1])
+		identifier, err := strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Printf("Error: Argument '%s' is not a valid draft identifier.\n", args[0])
+			os.Exit(-1)
+		}
+		Edit(identifier)
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(editCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// editCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// editCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
+	editCmd.SetArgs([]string{"identifier"})
 }
